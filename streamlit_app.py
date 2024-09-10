36645a91e079218eada91e4dc6b9cb8e174dcfff
Wells-L <wells.lucasp@gmail.com>
2024-09-10 18:56:32 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(f"O numero escrito é : {numero_1}")
 numero_2 = st.text_input("digite outro numero")
 
 st.write(f"O numero escrito é : {numero_2}")
-
+operacao = st.selectbox("Escolha a operação", ["soma", "subtração", "multiplicação", "divisão"])
 
 if numero_1 and numero_2: 
   soma = int(numero_1) + int(numero_2)
