a9c11ffa642aeb9f7a0db36fb9da11474c639b27
Wells-L <wells.lucasp@gmail.com>
2024-09-10 18:51:04 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,12 @@ st.write("sou o Lucas")
 
 numero_1 = st.text_input("digite um numero")
 
-st.write(f"O numero escrito é :{numero_1}")
+st.write(f"O numero escrito é : {numero_1}")
+
+numero_2 = st.text_input("digite outro numero")
+
+st.write(f"O numero escrito é : {numero_2}")
+
+soma = int(numero_1) + int(numero_2)
+
+st.write(f"A soma dos numeros é : {soma}")
