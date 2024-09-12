e7337e5b6f4863c15b568cee88d940b77a5058f4
Wells-L <wells.lucasp@gmail.com>
2024-09-12 18:21:13 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,39 +6,39 @@ st.title("Calculadora")
 st.write("Faça a sua conta")
 
 
+
 numero_1 = st.text_input("digite um numero")
-
-st.write(f"O numero escrito é : {numero_1}")
-
 numero_2 = st.text_input("digite outro numero")
 
-st.write(f"O numero escrito é : {numero_2}")
-
-operacao = st.selectbox("Escolha a operação", ["soma", "subtração", "multiplicação", "divisão"])
-
-if operacao == "soma":
-
-  soma = float(numero_1) + float(numero_2)
-  st.write(f"A soma dos numeros é : {soma}")
-
-elif operacao == "subtração":
-
-  subtração = float(numero_1) - float(numero_2)
-  st.write(f"A subtração dos numeros é : {subtração }")
-
-elif operacao == "multiplicação":
-
-  multiplicação = float(numero_1) * float(numero_2)
-  st.write(f"A multiplicação dos numeros é : {multiplicação }")
-
-elif operacao == "divisão":
-
-  divisão = float(numero_1) / float(numero_2)
-  st.write(f"A divisão dos numeros é : {divisão }")
 
 
+operacao = st.selectbox("Escolha a operação", ["soma", "subtração", "resultado", "divisão","potenciação","raiz quadrada"])
+resultado = None
 
+if numero_1 and numero_2:
 
+  if operacao == "soma":
 
+    resultado = float(numero_1) + float(numero_2)
+    st.write(f"A soma dos numeros é : {resultado}")
 
+  elif operacao == "subtração":
+
+    resultado = float(numero_1) - float(numero_2)
+    st.write(f"A subtração dos numeros é : {resultado }")
+
+  elif operacao == "multiplicação":
+
+    resultado = float(numero_1) * float(numero_2)
+    st.write(f"A multiplicação dos numeros é : {resultado }")
+
+  elif operacao == "divisão":
+
+    resultado = float(numero_1) / float(numero_2)
+    st.write(f"A divisao dos numeros é : {resultado }")
+
+  elif operacao == "potenciação":
+
+    resultado = float(numero_1) ** float(numero_2)
+    st.write(f"A potenciação dos numeros é : {resultado }")
 
