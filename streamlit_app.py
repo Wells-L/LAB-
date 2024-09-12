f3b87054ba15198db4d49819450cc95ddcf52a41
Wells-L <wells.lucasp@gmail.com>
2024-09-12 18:44:05 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,48 +3,55 @@ import streamlit as st
 import math as mt
 from math import sqrt
 
-st.title("Calculadora")
+# side bar para selecao de paginas 
+st.sidebar.title("menu do Lucas")
+page = st.sidebar.selectbox("escolha uma pagina",["Calculadora"] )
 
-st.write("Faça a sua conta")
+if page == "Calculadora":
+
+  #pagina de calculadora
+  st.title("Calculadora")
+
+  st.write("Faça a sua conta")
 
 
 
-numero_1 = st.text_input("digite um numero")
-numero_2 = st.text_input("digite outro numero")
+  numero_1 = st.text_input("digite um numero")
+  numero_2 = st.text_input("digite outro numero")
 
 
 
-operacao = st.selectbox("Escolha a operação", ["soma", "subtração","divisão","potenciação","raiz quadrada"])
-resultado = None
+  operacao = st.selectbox("Escolha a operação", ["soma", "subtração","divisão","potenciação","raiz quadrada"])
+  resultado = None
 
-if numero_1 and numero_2:
+  if numero_1 and numero_2:
 
-  if operacao == "soma":
+    if operacao == "soma":
 
-    resultado = float(numero_1) + float(numero_2)
-    st.write(f"A soma dos numeros é : {resultado}")
+      resultado = float(numero_1) + float(numero_2)
+      st.write(f"A soma dos numeros é : {resultado}")
 
-  elif operacao == "subtração":
+    elif operacao == "subtração":
 
-    resultado = float(numero_1) - float(numero_2)
-    st.write(f"A subtração dos numeros é : {resultado }")
+      resultado = float(numero_1) - float(numero_2)
+      st.write(f"A subtração dos numeros é : {resultado }")
 
-  elif operacao == "multiplicação":
+    elif operacao == "multiplicação":
 
-    resultado = float(numero_1) * float(numero_2)
-    st.write(f"A multiplicação dos numeros é : {resultado }")
+      resultado = float(numero_1) * float(numero_2)
+      st.write(f"A multiplicação dos numeros é : {resultado }")
 
-  elif operacao == "divisão":
+    elif operacao == "divisão":
 
-    resultado = float(numero_1) / float(numero_2)
-    st.write(f"A divisao dos numeros é : {resultado }")
+      resultado = float(numero_1) / float(numero_2)
+      st.write(f"A divisao dos numeros é : {resultado }")
 
-  elif operacao == "potenciação":
+    elif operacao == "potenciação":
 
-    resultado = float(numero_1) ** float(numero_2)
-    st.write(f"A potenciação dos numeros é : {resultado }")
+      resultado = float(numero_1) ** float(numero_2)
+      st.write(f"A potenciação dos numeros é : {resultado }")
 
-  elif operacao == "raiz quadrada":
+    elif operacao == "raiz quadrada":
 
-    resultado = sqrt(float(numero_1))
-    st.write(f"A raiz quadrada dos numeros é : {resultado }")
+      resultado = sqrt(float(numero_1))
+      st.write(f"A raiz quadrada dos numeros é : {resultado }")
