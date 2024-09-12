19504a5a39f49bca18708ed8a76ec246667ac8cd
Wells-L <wells.lucasp@gmail.com>
2024-09-12 02:27:48 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,20 @@ if operacao == "soma":
   soma = int(numero_1) + int(numero_2)
   st.write(f"A soma dos numeros é : {soma}")
 
+elif operacao == "subtração":
+  subtracao = int(numero_1) - int(numero_2)
+  st.write(f"A subtração dos numeros é : {subtracao}")
+
+elif operacao == "multiplicação":
+  multiplicacao = int(numero_1) * int(numero_2)
+  st.write(f"A multiplicação dos numeros é : {multiplicacao}")
+
+elif operacao == "divisão":
+  divisao = int(numero_1) / int(numero_2)
+  st.write(f"A divisão dos numeros é : {divisao}")
+
+  
+
+
+
 
