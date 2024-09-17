f51d58d40331ecc7e0f19276be2cbed5fea6b70b
Wells-L <wells.lucasp@gmail.com>
2024-09-17 18:18:11 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ elif page == "Conversor":
 elif page == "testes":
 
   st.title("Conversor 2")
-  st.selectbox("real","dolar")
+  st.selectbox(["real"],["dolar"])
 
   number = st.number_input("Insert a number")
   st.write("The current number is ", number)
