ad034a5de8b0961dbf259904c73f44c1393742e5
Wells-L <wells.lucasp@gmail.com>
2024-10-10 19:56:30 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -141,19 +141,6 @@ elif page == "graficos":
 
 
 elif page == "forca":
-  import random
-  lista_palavras = []
-  with open('content/palavras.txt', 'r') as palavras_file:
-      for line in palavras_file:
-          lista_palavras.append(line.strip())
-
-      palavra_secreta = random.choice(lista_palavras)
-
-      st.title("bem vindo ao jogo da forca")
-
-      st.write(f"A palavra tem {len(palavra_secreta)} letras.")
-      st.write(" ".join(palavra_chutada))
-
-      chute = st.text_input("Adivinha uma letra: ")
+  
 
       st.button("teste")
