c2bf9ac8539cc6f774de392aa671faec9e41bdb6
Wells-L <wells.lucasp@gmail.com>
2024-10-22 18:36:43 +0000

    New updates on 2024-10-22

--- view/forca.py
+++ view/forca.py
@@ -10,8 +10,12 @@ with open('content/palavras.txt', 'r') as palavras_file:
     for line in palavras_file:
         lista_palavras.append(line.strip())
 
+if "palavra_secreta" in st.session_state:
+  pass
+else:
+  st.session_state["palavra_secreta"] = random.choice(lista_palavras)
 
-palavra_secreta = random.choice(lista_palavras)
+palavra_secreta = st.session_state["palavra_secreta"]
 
 st.write(palavra_secreta)
 
@@ -20,4 +24,6 @@ chute = st.text_input("Esolha uma letra: ")
 
 if st.button("chute: "):
   st.write(chute)
+
+
 
