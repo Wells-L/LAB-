96f643c9bfafda1e0ed8ba41f11684948e1fdc78
Wells-L <wells.lucasp@gmail.com>
2024-10-22 18:11:49 +0000

    New updates on 2024-10-22

--- view/forca.py
+++ view/forca.py
@@ -1,4 +1,29 @@
 
+import random
 import streamlit as st
 
 st.title("forca")
+
+
+lista_palavras = []
+with open('content/palavras.txt', 'r') as palavras_file:
+    for line in palavras_file:
+        lista_palavras.append(line.strip())
+
+
+palavra_secreta = random.choice(lista_palavras)
+st.title("forca")
+st.write(palavra_secreta)
+
+if st.button("teste"):
+  st.write("Botão clicado!")
+
+if "contador" not in st.session_state:
+  st.session_state["contador"] = 0
+
+if st.button("somar"):
+  st.session_state["contador"] += 1
+st.write(f"o seu contador é igual a: {st.session_state["contador"]}")
+
+if st.button("resetar"):
+  st.session_state["contador"] = 0
