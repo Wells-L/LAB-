26bfd6eaaa0a4b4f4df16f8c2baeb7f03d3bab5e
Wells-L <wells.lucasp@gmail.com>
2024-10-24 20:00:42 +0000

    New updates on 2024-10-24

--- view/graficos.py
+++ view/graficos.py
@@ -11,7 +11,7 @@ df_leads = pd.read_excel(leads)
 st.write("dataframe")
 st.dataframe(df_leads)
 
-df_leads["dia da entrada"] = pd.to_datetime(df_leads["Dia de entrada"])
+df_leads["dia de entrada"] = pd.to_datetime(df_leads["Dia de entrada"])
 df_leads["dia"] = df_leads["dia de entrada"].dt.day
 
 groupby_leads_por_dia = df_leads.groupby("dia").agg({"ID do lead": "nunique}"}).reset_index()
