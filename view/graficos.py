ac1018be472747aa2407b5f57f4a3c2845e6223f
Wells-L <wells.lucasp@gmail.com>
2024-11-07 19:35:01 +0000

    New updates on 2024-11-07

--- view/graficos.py
+++ view/graficos.py
@@ -14,43 +14,48 @@ st.dataframe(df_leads)
 df_leads["Dia da entrada"] = pd.to_datetime(df_leads["Dia da entrada"])
 df_leads["Dia"] = df_leads["Dia da entrada"].dt.day
 
-groupby_leads_por_dia = df_leads.groupby("Dia").agg({"ID do lead": "nunique"}).reset_index()
+col1,col2 = st.columns(2)
 
-st.write("graficos de leads por dia")
+with col1 :
 
-grafico_leads_por_dia = px.line(
-    groupby_leads_por_dia,
-    x="Dia",
-    y="ID do lead",
-    labels={"ID do lead": "Quantia de Leads", "Dia":"Dia do mês"},
-    markers=True
-)
 
-st.plotly_chart(grafico_leads_por_dia)
+  groupby_leads_por_dia = df_leads.groupby("Dia").agg({"ID do lead": "nunique"}).reset_index()
+
+  st.write("graficos de leads por dia")
+
+  grafico_leads_por_dia = px.line(
+      groupby_leads_por_dia,
+      x="Dia",
+      y="ID do lead",
+      labels={"ID do lead": "Quantia de Leads", "Dia":"Dia do mês"},
+      markers=True
+  )
+
+  st.plotly_chart(grafico_leads_por_dia)
 
 #################################################
 #grafico de barras
 
+with col2 :
+
+  groupby_leads_por_unidade = (
+    df_leads
+    .groupby("Unidade")                  
+    .agg({"ID do lead": "nunique"})
+    .reset_index()
+  )  
 
 
-groupby_leads_por_unidade = (
-  df_leads
-  .groupby("Unidade")                  
-  .agg({"ID do lead": "nunique"})
-  .reset_index()
-)  
+  grafico_leads_por_unidade = px.bar(
+      groupby_leads_por_unidade,
+      x="Unidade",
+      y="ID do lead",
+      labels={"ID do lead": "Quantia de Leads", "Unidade":"Unidade"},
+      
+  )
 
-
-grafico_leads_por_unidade = px.bar(
-    groupby_leads_por_unidade,
-    x="Unidade",
-    y="ID do lead",
-    labels={"ID do lead": "Quantia de Leads", "Unidade":"Unidade"},
-    
-)
-
-st.plotly_chart(grafico_leads_por_unidade)
+  st.plotly_chart(grafico_leads_por_unidade)
 
 
 
-st.write("grafico por unidade")
+  st.write("grafico por unidade")
