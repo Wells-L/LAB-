25d01cf9ed34f2baa496dcd1e10c4210bd71b039
Wells-L <wells.lucasp@gmail.com>
2024-11-14 19:39:04 +0000

    New updates on 2024-11-14

--- view/graficos_sales.py
+++ view/graficos_sales.py
@@ -10,7 +10,9 @@ st.title("sales1")
 sales = "sales.xlsx"
 df_sales = pd.read_excel(sales)
 
-
+#parte2
+st.write("mostrando o frontend")
+st.dataframe(df_sales)
 
 #parte 3 a
 
@@ -19,6 +21,14 @@ df_sales["Data venda"] = pd.to_datetime(df_sales["Data venda"])
 #isolanado o doa do campo"data venda
 df_sales["Dia"] = df_sales["Data venda"].dt.day
 
-#parte2
-st.write("mostrando o frontend")
-st.dataframe(df_sales)
+#parte4 groupby vendas por dia
+
+groupby_vendas_por_dia = (
+    df_sales
+    .groupby("Dia")
+    .agg({"Valor líquido": "sum"})
+    .reset_index()
+  )
+
+st.write("mostrando vendas por dia ")
+st.dataframe(groupby_vendas_por_dia)
