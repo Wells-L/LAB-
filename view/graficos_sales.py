3b39fb8e3c70b3cab1287f9c50e530a2afa14908
Wells-L <wells.lucasp@gmail.com>
2024-11-14 19:27:58 +0000

    New updates on 2024-11-14

--- view/graficos_sales.py
+++ view/graficos_sales.py
@@ -4,3 +4,12 @@ import plotly.express as px
 import streamlit as st
 
 st.title("sales1")
+
+#parte1
+
+sales = "sales.xlsx"
+df_sales = pd.read_excel(sales)
+
+#parte2
+st.write("mostrando o frontrend")
+st.dataframe(df_sales)
