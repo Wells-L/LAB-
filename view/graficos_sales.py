152c4e5cf4552a290e2fafd01937e2998e8076c4
Wells-L <wells.lucasp@gmail.com>
2024-11-14 19:33:53 +0000

    New updates on 2024-11-14

--- view/graficos_sales.py
+++ view/graficos_sales.py
@@ -17,7 +17,7 @@ df_sales = pd.read_excel(sales)
 df_sales["Data venda"] = pd.to_datetime(df_sales["Data venda"])
 
 #isolanado o doa do campo"data venda
-df_sales["Dia"] = df_sales["Dia da entrada"].dt.day
+df_sales["Dia"] = df_sales["Data venda"].dt.day
 
 #parte2
 st.write("mostrando o frontend")
