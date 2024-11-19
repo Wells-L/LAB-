c8af3fd762cfcee09882d0ed15548946a5ed5caf
Wells-L <wells.lucasp@gmail.com>
2024-11-19 18:48:18 +0000

    New updates on 2024-11-19

--- view/graficos_sales.py
+++ view/graficos_sales.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.title("sales2")
+st.title("Informações de vendas")
 
 #parte1
 
@@ -11,11 +11,11 @@ sales = "sales.xlsx"
 df_sales = pd.read_excel(sales)
 
 #parte2
-st.write("mostrando o frontend")
+st.write("Dados de venda")
 st.dataframe(df_sales)
 
 #parte 3 a
-
+("receita por dia")
 df_sales["Data venda"] = pd.to_datetime(df_sales["Data venda"])
 
 #isolanado o doa do campo"data venda
